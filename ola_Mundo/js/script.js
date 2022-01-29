111ade8448b813b4c186541f686af3f42f27f07a
Elizangela <elizgomes08@gmail.com>
2022-01-29 15:27:09 +0000

    Repositorio do curso de logica com JavaScript

---
+++ ola_Mundo/js/script.js
@@ -0,0 +1,4 @@
+let nome=prompt("Digite o seu nome:")
+alert (`Ola ${nome}, seja bem vindo a imersao de Front End`)
+
+document.getElementById("saida").innerHTML=`Ola ${nome}, seja bem vindo a imersao de Front End`
